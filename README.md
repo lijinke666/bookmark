5ae1df11c2e060b4adf941f4d71bdd220efe73ef
Jinke.Li <1359518268@qq.com>
2018-07-02 12:04:40 +0800

    feat:rebalse

--- README.md
+++ README.md
@@ -4,11 +4,10 @@
 [![npm version](https://img.shields.io/npm/v/bookmask.svg?style=flat-square)](https://badge.fury.io/js/bookmask)
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg?style=flat-square)](https://www.npmjs.com/package/bookmask)
 
-
 :closed_book: generator bookmark form the directory
 
-You can quickly generate a directory page. 
-For example, there are many HTML pages in the folder, use bookmark build `index.html`, Then in gh-pages use it 
+You can quickly generate a directory page.
+For example, there are many HTML pages in the folder, use bookmark build `index.html`, Then in gh-pages use it
 
 ## Installation
 
@@ -25,15 +24,15 @@ $ [bm | bookmark] <command>
 ## Example
 
 ```
-bm -f "index.html" -d ./testDir -p ./publicPath
+bm -d /path/to/xx -o /page/to/xx -p './publicPath'
 ```
 
 ## Commands
 
 ```bash
   -v, --version            output the version number
-  -d --directory [value]   entry directory (default: __dirname)
-  -f --filename [value]    output file name (default: index.html)
+  -d --directory [value]   entry directory 
+  -o --output [value]      output directory 
   -p --publicPath [value]  output public path (default: ..)
   -h, --help               output usage information
 ```
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,23 +1,37 @@
-const path = require("path");
-const assert = require('assert')
-const coffee = require("coffee");
-const { version } = require("../package.json");
+const path = require('path');
+const fs = require('fs');
+const assert = require('assert');
+const coffee = require('coffee');
+const { version } = require('../package.json');
+const bookmark = path.resolve(__dirname, '../bin/bookmark');
 
-describe("bookmark Commands Tests", () => {
-  describe("#Commands", () => {
+describe('bookmark Commands Tests', () => {
+  describe('#Commands', () => {
     it(`should show version ${version}`, done => {
       coffee
-        .fork(path.resolve(__dirname, "../bin/bookmark"), ["-v"])
-        .expect("stdout", `${version}\n`)
-        .expect("code", 0)
+        .fork(bookmark, ['-v'])
+        .expect('stdout', `${version}\n`)
+        .expect('code', 0)
         .end(done);
     });
     it(`should show helper infos`, done => {
       coffee
-        .fork(path.resolve(__dirname, "../bin/bookmark"), ["-h"])
-        .expect("stdout", /^\n*\s*usage/i)
-        .expect("code", 0)
+        .fork(bookmark, ['-h'])
+        .expect('stdout', /^\n*\s*usage/i)
+        .expect('code', 0)
         .end(done);
+    });
+  });
+  describe('# bookmark',() => {
+    it(`shout generator index.html`, (done) => {
+      coffee
+        .fork(bookmark)
+        .expect('code', 0)
+        .expect('stdout', /index\.html/i)
+        .end(done);
+
+      const p = path.join(__dirname, '../bin');
+      assert(fs.existsSync(path.join(p, 'index.html')));
     });
   });
 });
